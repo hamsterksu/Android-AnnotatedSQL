e56ca1a079aaed535473ef96e04f53be180ef863
Gennadiy Dubina <hamsterksu@gmail.com>
2012-11-12 08:38:39 +0200

    fix bug with join to view

--- src/com/annotatedsql/processor/sql/SimpleViewProcessor.java
+++ src/com/annotatedsql/processor/sql/SimpleViewProcessor.java
@@ -1,5 +1,6 @@
 package com.annotatedsql.processor.sql;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +23,15 @@ public class SimpleViewProcessor {
 	 */
 	public static String create(Element c, Map<String, List<String>> tableColumns) {
 		HashMap<String, Element> aliases = new HashMap<String, Element>();
+		ArrayList<String> selectColumns = new ArrayList<String>();
+		
 		SimpleView view = c.getAnnotation(SimpleView.class);
 		String name = view.value();
-
+		if(tableColumns.containsKey(name)){
+			throw new AnnotationParsingException(String.format("Table/View with name '%s' alredy defined", name), c);
+		}
+		tableColumns.put(name, selectColumns);
+		
 		List<? extends Element> fields = c.getEnclosedElements();
 		
 		final StringBuilder select = new StringBuilder(fields.size() * 32);
@@ -37,18 +44,17 @@ public class SimpleViewProcessor {
 		for (Element f : fields) {
 			From tmpFrom = f.getAnnotation(From.class);
 			if (tmpFrom != null){
-				from = proceedFrom(tableColumns, aliases, select, sql, pos,
-						from, f, tmpFrom);
+				from = proceedFrom(tableColumns, aliases, select, sql, pos, from, f, tmpFrom, selectColumns);
 			}else{
 				Join join = f.getAnnotation(Join.class);
 				if(join != null){
-					proceedJoin(tableColumns, aliases, select, sql, f, join);
+					proceedJoin(tableColumns, aliases, select, sql, f, join, selectColumns);
 				}
 				RawJoin rawJoin = f.getAnnotation(RawJoin.class);
 				if(rawJoin != null){
 					if(join != null)
 						throw new AnnotationParsingException("element can have only one join", f);
-					proceedRawJoin(tableColumns, aliases, select, sql, f, rawJoin);
+					proceedRawJoin(tableColumns, aliases, select, sql, f, rawJoin, selectColumns);
 				}
 			}
 		}
@@ -62,7 +68,7 @@ public class SimpleViewProcessor {
 
 	private static void proceedJoin(Map<String, List<String>> tableColumns,
 			HashMap<String, Element> aliases, final StringBuilder select,
-			final StringBuilder sql, Element f, Join join) {
+			final StringBuilder sql, Element f, Join join,  ArrayList<String> selectColumns) {
 		Columns columns = f.getAnnotation(Columns.class);
 		String[] selectedColumns = columns != null ? columns.value() : null;
 		String asName = (String)((VariableElement)f).getConstantValue();
@@ -87,7 +93,7 @@ public class SimpleViewProcessor {
 		.append(" ON ").append(asName).append('.').append(join.joinColumn())
 		.append(" = ").append(join.onTable()).append('.').append(join.onColumn());
 		try{
-			addTableColumn(select, tableColumns.get(join.joinTable()), selectedColumns, asName, false);
+			addTableColumn(select, tableColumns.get(join.joinTable()), selectedColumns, asName, false, selectColumns);
 		}catch (RuntimeException e) {
 			throw new AnnotationParsingException(e.getMessage(), f);
 		}
@@ -95,7 +101,7 @@ public class SimpleViewProcessor {
 	
 	private static void proceedRawJoin(Map<String, List<String>> tableColumns,
 			HashMap<String, Element> aliases, final StringBuilder select,
-			final StringBuilder sql, Element f, RawJoin join) {
+			final StringBuilder sql, Element f, RawJoin join,  ArrayList<String> selectColumns) {
 		Columns columns = f.getAnnotation(Columns.class);
 		String[] selectedColumns = columns != null ? columns.value() : null;
 		String asName = (String)((VariableElement)f).getConstantValue();
@@ -119,7 +125,7 @@ public class SimpleViewProcessor {
 		}
 		sql.append(join.joinTable()).append(" AS ").append(asName).append(" ON ").append(join.onCondition());
 		try{
-			addTableColumn(select, tableColumns.get(join.joinTable()), selectedColumns, asName, false);
+			addTableColumn(select, tableColumns.get(join.joinTable()), selectedColumns, asName, false, selectColumns);
 		}catch (RuntimeException e) {
 			throw new AnnotationParsingException(e.getMessage(), f);
 		}
@@ -128,7 +134,7 @@ public class SimpleViewProcessor {
 	private static From proceedFrom(Map<String, List<String>> tableColumns,
 			HashMap<String, Element> aliases, final StringBuilder select,
 			final StringBuilder sql, final int pos, From from, Element f,
-			From tmpFrom) {
+			From tmpFrom, ArrayList<String> selectColumns) {
 		if(from != null){
 			throw new AnnotationParsingException("Dublicate @From annotation", f);
 		}
@@ -138,7 +144,7 @@ public class SimpleViewProcessor {
 		
 		String asName = (String)((VariableElement)f).getConstantValue();
 		try{
-			addTableColumn(select, tableColumns.get(from.value()), selectedColumns, asName, true);
+			addTableColumn(select, tableColumns.get(from.value()), selectedColumns, asName, true, selectColumns);
 		}catch (RuntimeException e) {
 			throw new AnnotationParsingException(e.getMessage(), f);
 		}
@@ -147,7 +153,7 @@ public class SimpleViewProcessor {
 		return from;
 	}
 	
-	private static void addTableColumn(StringBuilder select, List<String> coluumns, String[] selectedColumns, String asName, boolean ignoreId){
+	private static void addTableColumn(StringBuilder select, List<String> coluumns, String[] selectedColumns, String asName, boolean ignoreId, ArrayList<String> selectColumns){
 		if(coluumns == null || coluumns.isEmpty())
 			return;
 		if(selectedColumns != null && selectedColumns.length != 0){
@@ -155,20 +161,23 @@ public class SimpleViewProcessor {
 				if(!coluumns.contains(selCol)){
 					throw new RuntimeException("Table doesn't have column '" + selCol + "'");
 				}
-				addColumn(select, asName, ignoreId, selCol);
+				addColumn(select, asName, ignoreId, selCol, selectColumns);
 			}
 		}else{
 			for(String c : coluumns){
-				addColumn(select, asName, ignoreId, c);
+				addColumn(select, asName, ignoreId, c, selectColumns);
 			}
 		}
 	}
 
-	private static void addColumn(StringBuilder select, String asName, boolean ignoreId, String c) {
+	private static void addColumn(StringBuilder select, String asName, boolean ignoreId, String c, ArrayList<String> selectColumns) {
 		if(ignoreId && "_id".equals(c)){
+			selectColumns.add(c);
 			select.append(", ").append(asName).append('.').append(c);
 		}else{
-			select.append(", ").append(asName).append('.').append(c).append(" as ").append(asName).append('_').append(c);
+			String fullName = asName + '_' + c;
+			selectColumns.add(fullName);
+			select.append(", ").append(asName).append('.').append(c).append(" as ").append(fullName);
 		}
 	}
 	
