fd8fafc9fa53315f1c8959f4465eab12c14f7afa
Julien Banse <julien.banse@gmail.com>
2014-09-29 23:00:32 +0200

    remove data type field to use java class type

--- android-annotatedsql-api/src/main/java/com/annotatedsql/annotation/sql/Column.java
+++ android-annotatedsql-api/src/main/java/com/annotatedsql/annotation/sql/Column.java
@@ -1,17 +1,23 @@
 package com.annotatedsql.annotation.sql;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.CLASS)
 @Target(ElementType.FIELD)
 public @interface Column {
-	
-	static final String EMPTY_DEF_VAL = "";
-	
-	Type type();
-	String defVal() default EMPTY_DEF_VAL; 
 
-	public static enum Type{
-		INTEGER, REAL, TEXT, BLOB 
-	}
+    static final String EMPTY_DEF_VAL = "";
+
+//    Type type();
+
+    String defVal() default EMPTY_DEF_VAL;
+
+    Class javaClass();
+
+    public static enum Type {
+        INTEGER, REAL, TEXT, BLOB
+    }
 }
