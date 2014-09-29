1b69a031e52d1565fc97257a81a6cfee715548cc
Julien Banse <julien.banse@gmail.com>
2014-09-29 23:02:35 +0200

    add method to get Camelcase String values

--- android-annotatedsql-processor/src/main/java/com/annotatedsql/util/TextUtils.java
+++ android-annotatedsql-processor/src/main/java/com/annotatedsql/util/TextUtils.java
@@ -1,29 +1,39 @@
 package com.annotatedsql.util;
 
+import org.apache.commons.lang.WordUtils;
+
 public class TextUtils {
 
-	public static boolean isEmpty(String str){
-		return str == null || str.length() == 0;
-	} 
-	
-	public static String capitalize(String str){
-		if(str == null || str.length() == 0)
-			return str;
-		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-	}
-	
-	public static String var2class(String str){
-		if(str == null || str.length() == 0)
-			return str;
-		String[] split = str.split("_");
-		if(split.length > 1){
-			StringBuilder builder = new StringBuilder();
-			for(String part : split){
-				builder.append(capitalize(part));
-			}
-			return builder.toString();
-		}else{
-			return str;
-		}
-	}
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    public static String capitalize(String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+
+
+    public static String convertInCamelCase(String name) {
+        return WordUtils.capitalizeFully(name, new char[]{'_'}).replaceAll("_", "");
+    }
+
+
+    public static String var2class(String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        String[] split = str.split("_");
+        if (split.length > 1) {
+            StringBuilder builder = new StringBuilder();
+            for (String part : split) {
+                builder.append(capitalize(part));
+            }
+            return builder.toString();
+        } else {
+            return str;
+        }
+    }
 }
