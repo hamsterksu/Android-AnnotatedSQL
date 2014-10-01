c1ae3ad4d5c650b6bef7d2c57147447c7cdc8821
Julien Banse <julien.banse@gmail.com>
2014-10-01 23:05:06 +0200

    set member as public

--- android-annotatedsql-processor/src/main/java/com/annotatedsql/ftl/ColumnWrapperMeta.java
+++ android-annotatedsql-processor/src/main/java/com/annotatedsql/ftl/ColumnWrapperMeta.java
@@ -4,8 +4,8 @@ package com.annotatedsql.ftl;
  * Created by jbanse on 01/10/2014.
  */
 public class ColumnWrapperMeta {
-    final String name;
-    final String classType;
+    public final String name;
+    public final String classType;
 
     public ColumnWrapperMeta(String name, String clazz) {
         super();
